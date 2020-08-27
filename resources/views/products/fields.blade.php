3caddb663b0fcc1d54367712cd0ba24eb81687a5
Rizal Faozi <ryzal.kazama@gmail.com>
2020-08-27 16:19:09 +0700

    update noted

--- resources/views/products/fields.blade.php
+++ resources/views/products/fields.blade.php
@@ -69,6 +69,12 @@
     {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
 </div>
 
+<!-- Description Field -->
+<div class="form-group col-sm-12 col-lg-12">
+    {!! Form::label('noted', 'Noted:') !!}
+    {!! Form::textarea('noted', null, ['class' => 'form-control']) !!}
+</div>
+
 <!-- Motif Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('theme', 'Motif:') !!}
